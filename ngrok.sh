f26fbef52a0d7664143d6696287320268d369cbc
Hussnain Amjad <95158642+Hussnain12@users.noreply.github.com>
2024-02-13 14:51:10 +0500

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -11,5 +11,4 @@ echo "======================="
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 read -p "Paste Ngrok Authtoken: " CRP
 ngrok config add-authtoken $CRP
-unzip ngrok.zip > /dev/null 2>&1
 
