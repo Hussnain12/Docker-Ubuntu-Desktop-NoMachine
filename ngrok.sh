94fce773f0772c8eacd1856c9a3553f9c0bf1545
Hussnain Amjad <95158642+Hussnain12@users.noreply.github.com>
2024-02-13 14:50:55 +0500

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -8,6 +8,8 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok..."
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+read -p "Paste Ngrok Authtoken: " CRP
+ngrok config add-authtoken $CRP
 unzip ngrok.zip > /dev/null 2>&1
 
